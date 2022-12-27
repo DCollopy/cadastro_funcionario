e3e59bcce4843b01f5b394817ab4cc3ef271e2b5
DCollopy <damaris.collopy@al.infnet.edu.br>
2022-12-27 13:47:51 -0300

    resolvendo relacao parental poom

--- web/src/main/java/br/com/cadastro/web/WebApplication.java
+++ web/src/main/java/br/com/cadastro/web/WebApplication.java
@@ -1,4 +1,4 @@
-package com.br.web;
+package br.com.cadastro.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- web/src/test/java/br/com/cadastro/web/WebApplicationTests.java
+++ web/src/test/java/br/com/cadastro/web/WebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.br.web;
+package br.com.cadastro.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
