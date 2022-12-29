161d16bdb11ef0c2c55f68c5cf13299ef8a3cc32
DCollopy <damaris.collopy@al.infnet.edu.br>
2022-12-29 17:00:53 -0300

    TDD

--- dominio/src/test/java/br/com/cadastro/dominio/FuncionarioTeste.java
+++ dominio/src/test/java/br/com/cadastro/dominio/FuncionarioTeste.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FuncionarioTeste {
+public class FuncionarioTeste {
 
     protected FuncionarioValidaTeste funcionarioValidaTeste = new FuncionarioValidaTeste();
 
